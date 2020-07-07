9b469529ca63981a401b65d933ec97dbbace0752
Matheus Leal <matheus.leal@ccc.ufcg.edu.br>
2020-07-07 10:31:42 -0300

    Adiciona requisição de search

--- src/controllers/contratos.controller.js
+++ src/controllers/contratos.controller.js
@@ -14,12 +14,14 @@ const SUCCESS = 200;
 // Retorna todas os contratos de um município 
 exports.getContratosPorMunicipio = (req, res) => {
     const cd_municipio = req.query.cd_municipio
-    
-    Contrato.findAll({ where: {
-        cd_municipio: cd_municipio
-    }})
-    .then(contratos => res.status(SUCCESS).json(contratos))
-    .catch(err => res.status(BAD_REQUEST).json({ err }));
+
+    Contrato.findAll({
+        where: {
+            cd_municipio: cd_municipio
+        }
+    })
+        .then(contratos => res.status(SUCCESS).json(contratos))
+        .catch(err => res.status(BAD_REQUEST).json({ err }));
 };
 
 // Recupera o contrato pelo ID
@@ -31,8 +33,8 @@ exports.getContratoById = (req, res) => {
             id_contrato: id
         }
     })
-    .then(contratos => res.json(contratos))
-    .catch(err => res.status(BAD_REQUEST).json({ err }));
+        .then(contratos => res.json(contratos))
+        .catch(err => res.status(BAD_REQUEST).json({ err }));
 }
 
 // Recupera todos os contratos de uma licitação
@@ -44,6 +46,36 @@ exports.getContratosByLicitacao = (req, res) => {
             id_licitacao: id_licitacao
         }
     })
-    .then(contratos => res.json(contratos))
-    .catch(err => res.status(BAD_REQUEST).json({ err }));
+        .then(contratos => res.json(contratos))
+        .catch(err => res.status(BAD_REQUEST).json({ err }));
+}
+
+exports.getContratosByQuery = (req, res) => {
+    let query = 'SELECT \
+        id_contrato, \
+            de_obs \
+        FROM \
+            ( \
+                SELECT \
+                *, \
+                to_tsvector( \
+                    contrato.language:: regconfig, \
+                    contrato.de_obs \
+                ) AS document \
+            FROM \
+                contrato \
+            ) p_search \
+        WHERE \
+        p_search.document @@to_tsquery("portuguese", "hospital") \
+        ORDER BY \
+        ts_rank( \
+            p_search.document, \
+            to_tsquery("portuguese", "hospital") \
+        ) DESC; '
+
+    models.sequelize.query(query, {
+        model: Contrato,
+        mapToModel: true
+    }).then(Contrato => res.status(SUCCESS).json(Contrato))
+        .catch(err => res.status(BAD_REQUEST).json({ err }));
 }
